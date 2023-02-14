673b058eb461d576552d149e3c4b9deb16d98ab0
stormymayday <stormymayday7@gmail.com>
2023-02-13 20:59:10 -0700

    Added event listener to toggleBtn that toggles the 'show-sidebar' class on 'click'.

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const toggleBtn = document.querySelector('.sidebar-toggle');
 const sidebar = document.querySelector('.sidebar');
 const closeBtn = document.querySelector('.close-btn');
+
+toggleBtn.addEventListener('click', function () {
+
+    // add / remove methods:
+    // if (sidebar.classList.contains('show-sidebar')) {
+    //     sidebar.classList.remove('show-sidebar');
+    // } else {
+    //     sidebar.classList.add('show-sidebar');
+    // }
+
+    // toggle method:
+    sidebar.classList.toggle('show-sidebar');
+
+});
